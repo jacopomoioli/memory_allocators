902cce968b843087aa16c416e86bdab29149ae67
Jacopo Moioli <me@jacopomoioli.com>
2025-08-28 19:35:55 +0200

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# memory_allocators
+# Memory Allocators
+
+## Linear Allocator
+Allocates memory sequentially from a fixed buffer. Memory must be freed in LIFO order.
+Uses a single pointer that advances for alloc() and resets on afree().
+
+Inspired by R&K chapter 5.4.
---
+++ linear_allocator.c
@@ -0,0 +1,38 @@
+/*
+* Linear Allocator
+*
+* Allocates memory sequentially from a fixed buffer. Memory must be freed in LIFO order.
+* Uses a single pointer that advances for alloc() and resets on afree()
+*
+*/
+
+#include <stdio.h>
+
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE];
+static char* allocp = allocbuf;
+
+char* alloc(int size);
+void afree(char* pointer);
+
+int main(int argc, char** argv){
+    char* pointer = alloc(10);
+    printf("Base addr: 0x%p\nNext base addr: 0x%p\n\n", pointer, allocp);
+    afree(pointer);
+    printf("Base addr: 0x%p\nNext base addr: 0x%p\n\n", pointer, allocp);
+
+    return 0;
+}
+
+char* alloc(int size){
+    if((allocp + size) > (allocbuf + ALLOCSIZE)){
+        return 0;
+    }
+    allocp += size;
+    return allocp - size;
+}
+
+void afree(char* pointer){
+    allocp = pointer;
+}
